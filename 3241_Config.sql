88afb019aed327b37da7b1366524d900ad513f70
SnowySailor <kanel.nick@gmail.com>
2018-02-15 16:43:06 -0500

    Drop database before create. Remove index on garage.floor
    Better formatting inside the Config file to improve readability.

--- 3241_Config.sql
+++ 3241_Config.sql
@@ -1,36 +1,36 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 
+DROP DATABASE IF EXISTS       `cse3241_project`;
 CREATE DATABASE IF NOT EXISTS `cse3241_project`;
 USE `cse3241_project`;
 
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-    `id` int unsigned PRIMARY KEY,
-    `user_group` int NOT NULL,
-    `name` varchar(30) NOT NULL,
-    `login_name` nvarchar(100) NOT NULL,
-    `password` varchar(60) NOT NULL
+    `id`           int unsigned  PRIMARY KEY,
+    `user_group`   int           NOT NULL,
+    `name`         varchar(30)   NOT NULL,
+    `login_name`   nvarchar(100) NOT NULL,
+    `password`     varchar(60)   NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `garage`;
 
 CREATE TABLE `garage` (
-    `id` int unsigned PRIMARY KEY,
-    `name` varchar(30) NOT NULL,
-    `managed_by` int unsigned,
+    `id`           int unsigned PRIMARY KEY,
+    `name`         varchar(30)  NOT NULL,
+    `managed_by`   int unsigned,
     CONSTRAINT `managed_by_user_fk` FOREIGN KEY (`managed_by`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `parking_spot`;
 
 CREATE TABLE `parking_spot` (
-    `garage_id` int unsigned NOT NULL,
-    `floor_no` int unsigned NOT NULL,
-    `spot_no` int unsigned NOT NULL,
-    `state` int NOT NULL DEFAULT 1,
+    `garage_id`    int unsigned NOT NULL,
+    `floor_no`     int unsigned NOT NULL,
+    `spot_no`      int unsigned NOT NULL,
+    `state`        int          NOT NULL DEFAULT 1,
     PRIMARY KEY (`garage_id`, `floor_no`, `spot_no`),
-    KEY `floor_no_idx` (`floor_no`),
     CONSTRAINT `garage_id_fk` FOREIGN KEY (`garage_id`) REFERENCES `garage` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
