f035992f7e8811ce8aca956924151ebcd429a259
SnowySailor <kanel.nick@gmail.com>
2018-03-25 02:51:12 -0400

    Better semaphore keys. Destruct function

--- dba.php
+++ dba.php
@@ -48,14 +48,28 @@ class ConnectionPool {
 
     function __construct($sClassName, ...$aParams) {
         // Create a key for each semaphore
-        $this->_iAvailableLockKey = 1;
-        $this->_iUsedLockKey = 2;
+        // Get this object's hash
+        $sThisHash = spl_object_hash($this);
+        // Take the last 6 characters from the MD5 hash
+        // We do this because two objects can have very similar hashes
+        // but only be different by a character or two in the middle
+        // Example: 000000003cc56d770000000007fa48c5
+        //      vs. 000000003cc56d0d0000000007fa48c5
+        $iBaseKey = hexdec(substr(md5($sThisHash), strlen($sThisHash)-8));
+        $this->_iAvailableLockKey = $iBaseKey;
+        $this->_iUsedLockKey = $iBaseKey+1;
         // Set up both semaphores
         $this->_lAvailableLock = sem_get($this->_iAvailableLockKey, 1);
         $this->_lUsedLock = sem_get($this->_iUsedLockKey, 1);
         // Set up the connection data
         $this->_sClassName = $sClassName;
         $this->_aCredentials = $aParams;
+    }
+
+    function __destruct() {
+        // Remove semaphores
+        sem_remove($this->_lAvailableLock);
+        sem_remove($this->_lUsedLock);
     }
 
     private function newConnection() {
