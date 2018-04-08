4bcce9fd4102d441421f49ea82fd2f69da1ca66c
SnowySailor <kanel.nick@gmail.com>
2018-04-08 19:51:35 -0400

    Add event. Update FK constraint names.

--- 3241_Config.sql
+++ 3241_Config.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `garage` (
     `region`       nvarchar(50)      NULL DEFAULT '',
     `country`      nvarchar(50)  NOT NULL DEFAULT '',
     `managed_by`   int unsigned,
-    CONSTRAINT `managed_by_user_fk` FOREIGN KEY (`managed_by`) REFERENCES `user` (`id`)
+    CONSTRAINT `garage:managed_by` FOREIGN KEY (`managed_by`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `parking_spot` (
@@ -28,7 +28,29 @@ CREATE TABLE IF NOT EXISTS `parking_spot` (
     `spot_no`      int unsigned NOT NULL,
     `state`        int          NOT NULL DEFAULT 1,
     PRIMARY KEY (`garage_id`, `floor_no`, `spot_no`),
-    CONSTRAINT `garage_id_fk` FOREIGN KEY (`garage_id`) REFERENCES `garage` (`id`)
+    CONSTRAINT `parking_spot:garage_id` FOREIGN KEY (`garage_id`) REFERENCES `garage` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `report_data` (
+    `garage_id`    int unsigned NOT NULL,
+    `time`         datetime     NOT NULL,
+    `data`         mediumtext   NOT NULL,
+    PRIMARY KEY (`garage_id`, `time`),
+    CONSTRAINT `report_data:garage_id` FOREIGN KEY (`garage_id`) REFERENCES `garage` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DELIMITER //
+CREATE EVENT IF NOT EXISTS `report_data`
+ON SCHEDULE
+-- Run every day starting at the next midnight
+EVERY 1 DAY
+STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 1 DAY + INTERVAL 1 HOUR)
+DO
+BEGIN
+    INSERT INTO `report_data` (`garage_id`, `time`, `data`)
+    SELECT G.id, UTC_TIMESTAMP(), CONCAT('[', GROUP_CONCAT(JSON_OBJECT('Floor', P.floor_no, 'Spot', P.spot_no, 'State', state)), ']')
+    FROM `garage` G INNER JOIN parking_spot P ON G.id = P.garage_id GROUP BY G.id;
+END //
+DELIMITER ;
 
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
