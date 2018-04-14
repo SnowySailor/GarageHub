74b052dc0a1e8753fb39dcb5acccb7a1bf89b710
SnowySailor <kanel.nick@gmail.com>
2018-04-13 21:36:22 -0400

    Garage page additions

--- website/garage.php
+++ website/garage.php
@@ -3,67 +3,92 @@
 if (!defined('PROJECT_ONLINE')) exit('No dice!');
 
 function getGaragePage() {
-    $iGarageId = CSE3241::tryParseInt(CSE3241::getRequestParam('garageid'));
+    // Parse the garage id
+    $iGarageId = CSE3241::tryParseInt(CSE3241::getRequestParam('garageid'), -1);
+    // Check to see if there were errors parsing or if the user isn't allowed to see the garage
     if ($iGarageId < 0 || !CSE3241::isUserAuthForGarage($iGarageId)) {
         CSE3241::failBadRequest('Garage id does not exist or not authorized');
     }
 
-    // Show stats for garage
-    // Show floors
-    //$aGarage = CSE3241::database()->select('*')
+    $sContent = '';
 
+    // Get all state counts for this garage
     $aStateCounts = CSE3241::database()->select('count(*) as c, state')
                             ->from('parking_spot')
                             ->where(array('garage_id' => $iGarageId))
                             ->groupBy('state')
                             ->orderBy('state')
                             ->execute('getRows');
-    // available = 1, in-use = 2, out-of-service = 3
-    $sContent = "<table>";
-    $sContent   .= "<tr>";
-        $sContent   .= "<th>State</th>";
-        $sContent   .= "<th>Count</th>";
-    $sContent   .= "</tr>";
-    foreach ($aStateCounts as $aC) {
-        $sState = getStateName($aC["state"]);
-        $iCount = $aC["c"];
-
-        $sContent .= "<tr>";
-            $sContent .= "<td>" . $sState . "</td>";
-            $sContent .= "<td>" . $iCount . "</td>";
-        $sContent .= "</tr>";
-    }
-    $sContent .= "</table>";
-
-
+    // Get all floors for the garage
     $aFloors = CSE3241::database()->select('distinct(floor_no)')
                         ->from('parking_spot')
                         ->where(array('garage_id' => $iGarageId))
                         ->execute('getRows');
 
-    var_dump($aFloors);
+    // Make a table for the states of spots in the garage
+    $sContent .= makeStateTable($aStateCounts);
+
+    // Listing of floors in garage
+    $sContent .= '<table>';
+    $sContent   .= '<tr>';
+        $sContent   .= '<th>Floor</th>';
+    $sContent   .= '</tr>';
+    foreach ($aFloors as $aF) {
+        $iFloorNo = $aF['floor_no'];
+
+        $sContent .= '<tr>';
+            $sContent .= '<td onclick="onclickGarageFloor(' . $iGarageId . ', ' . $iFloorNo . ')">Floor ' . $iFloorNo . '</td>';
+        $sContent .= '</tr>';
+    }
+    $sContent .= '</table>';
+
     return $sContent;
 }
 
-function getStateName($iState) {
-    switch ($iState) {
-        case 1:
-            return 'Available';
-        case 2:
-            return 'In Use';
-        case 3:
-            return 'Out Of Service';
-        default:
-            return '';
+function getGarageFloorPage() {
+    $iGarageId = CSE3241::tryParseInt(CSE3241::getRequestParam('garageid'), -1);
+    $iFloorId = CSE3241::tryParseInt(CSE3241::getRequestParam('floorid'), -1);
+    if ($iFloorId < 0 || $iGarageId < 0 || !CSE3241::isUserAuthForGarage($iGarageId)) {
+        CSE3241::failBadRequest('Floor id does not exist or not authorized');
     }
-    return '';
-}
 
-function getGarageFloor($iGarageId, $iFloorNo) {
-    // Show stats for floor
-    // Show spots
-    // Allow marking of specific spots as out of service and back
-    // Allow marking of entire floor as out of service
+    $sContent = '';
+
+    // Get all counts for each state
+    $aStateCounts = CSE3241::database()->select('count(*) as c, state')
+                        ->from('parking_spot')
+                        ->where(array('garage_id' => $iGarageId, 'floor_no' => $iFloorId))
+                        ->groupBy('state')
+                        ->orderBy('state')
+                        ->execute('getRows');
+
+    // Get the garage name
+    $sGarageName = CSE3241::database()->select('name')
+                            ->from('garage')
+                            ->where(array('id' => $iGarageId))
+                            ->execute('getField');
+
+    // Get each spot on the floor
+    $aSpots = CSE3241::database()->select('*')
+                        ->from('parking_spot')
+                        ->where(array('garage_id' => $iGarageId, 'floor_no' => $iFloorId))
+                        ->orderBy('spot_no')
+                        ->execute('getRows');
+
+    // Get page's subheader
+    $sContent .= makePageSubheader($sGarageName . ' - Floor ' . $iFloorId);
+
+    // Get the state counts table
+    $sContent .= makeStateTable($aStateCounts);
+
+    // Render the close floor button
+    $sContent .= '<input id="closefloorbtn" value="Close Floor" type="button" onclick="onclickCloseGarageFloor(' . $iGarageId . ',' .$iFloorId .')"/>';
+
+    // Make table of spots
+    $sContent .= makeSpotGrid($aSpots);
+
+    // Return content
+    return $sContent;
 }
 
 function showGarageTable() {
@@ -118,6 +143,104 @@ function makeAddressDisplay($aGarage) {
     $sAddress .= !is_null($aGarage['city']) ? (strlen($aGarage['address']) > 0 ? ', ' : '') . $aGarage['city'] : '';
     $sAddress .= !is_null($aGarage['region']) ? (strlen($aGarage['city']) > 0 ? ', ' : '') . $aGarage['region'] : '';
     return $sAddress;
+}
+
+function makeStateTable($aStateCounts) {
+    // available = 1, in-use = 2, out-of-service = 3
+    $aValidStates = array(true,true,true);
+
+    $sContent = '<table>';
+    $sContent   .= '<tr>';
+        $sContent   .= '<th>State</th>';
+        $sContent   .= '<th>Count</th>';
+    $sContent   .= '</tr>';
+    
+    foreach ($aStateCounts as $aC) {
+        $aValidStates[$aC['state'] - 1] = false;
+        $sState = getStateName($aC['state']);
+        $iCount = $aC['c'];
+        $sContent .= '<tr>';
+            $sContent .= '<td>' . $sState . '</td>';
+            $sContent .= '<td>' . $iCount . '</td>';
+        $sContent .= '</tr>';
+    }
+
+    for ($i = 0; $i <= 2; $i++) {
+        if ($aValidStates[$i] == true) {
+            $sState = getStateName($i+1);
+            
+            $sContent .= '<tr>';
+                $sContent .= '<td>' . $sState . '</td>';
+                $sContent .= '<td>0</td>';
+            $sContent .= '</tr>';
+        }
+    }
+    $sContent .= '</table>';
+    return $sContent;
+}
+
+function makeSpotGrid($aSpots, $sId = '', $sTableClass = '', $sRowClass = '', $sTDClass = '') {
+    if ($sId == '') { $sId = 'spottable'; }
+    // 20 spots in each row
+    $iWidth = 20;
+
+    $sContent = '';
+    $sContent .= '<table id="' . $sId . '">';
+
+    for ($i = 0; $i < count($aSpots)/$iWidth; $i++) {
+        // New row every {$iWidth} spots
+        $sContent .= '<tr class="' . $sRowClass . '">';
+        for ($j = 0; $j < $iWidth; $j++) {
+            // Make sure we don't go too far
+            if (($iWidth * $i + $j) >= count($aSpots)) { break; }
+            // Get the spot
+            $aSpot = $aSpots[($iWidth * $i) + $j];
+            // Get the class for coloration
+            $sStateClass = getStateClass($aSpot['state']);
+
+            // Generate the spot itself
+            $sContent .= '<td class="' . $sTDClass . '">';
+            $sContent   .= '<div onclick="onclickSpot(' . $aSpot['state'] . ',' . $aSpot['garage_id'] . ',' . $aSpot['floor_no'] . ',' . $aSpot['spot_no'] . ')" class="parkingspot ' . $sStateClass . '">'; 
+            $sContent     .= $aSpot['spot_no'];
+            $sContent   .= '</div>';
+            $sContent .= '</td>';
+        }
+        $sContent .= '</tr>';
+    }
+    return $sContent;
+}
+
+function makePageSubheader($sText) {
+    if (is_null($sText) || !is_string($sText)) { $sText = ''; }
+    return '<div class="pagesubheader">' . $sText . '</div>';
+}
+
+function getStateName($iState) {
+    switch ($iState) {
+        case 1:
+            return 'Available';
+        case 2:
+            return 'In Use';
+        case 3:
+            return 'Out Of Service';
+        default:
+            return '';
+    }
+    return '';
+}
+
+function getStateClass($iState) {
+    switch ($iState) {
+        case 1:
+            return 'available';
+        case 2:
+            return 'inuse';
+        case 3:
+            return 'outofservice';
+        default:
+            return '';
+    }
+    return '';
 }
 
 ?>
